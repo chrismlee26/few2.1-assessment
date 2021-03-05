a91166baeb6ae2195d4f4f3e34ac67b5c453ef17
CHRIS_LEE <chrismlee87@gmail.com>
2021-03-04 16:21:36 -0800

    up to challenge 4

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Challenge 0
+const { format } = require('prettier');
 const data = require('./data.json');
+const Lib = require('@chrisle26/homework-1-strings');
+const D = require('few2-1-date-library');
 // console.log(data)
 
 // Creating a function that loops & creates items for later functions to change.
@@ -21,10 +24,50 @@ function displayData(data) {
   return arrayOfItems
 }
 
-console.log(displayData(data))
+// console.log(displayData(data))
 displayData(data)
 
-// Challenge 1
-function firstAndLast(arrayOfItems) {
 
+
+// // Challenge 1
+// function capitalizeName(str) {
+//   return str[0].toUpperCase() + str.slice(1);
+// } 
+
+// console.log(capitalizeName(firstNameTest))
+// console.log(capitalizeName(lastNameTest))
+
+const firstNameTest = data[0].first_name
+const lastNameTest = data[0].last_name
+
+function printFirstLastName() {
+  let output = Lib.capitalize(firstNameTest) + " " + Lib.capitalize(lastNameTest);
+  console.log(output)
+  return output
 }
+
+printFirstLastName()
+
+const purchasedTest = data[0].purchased
+
+// Challenge 2
+function formatDateString() {
+  const output = new D(purchasedTest).format('M, t, Y')
+  let carPurchasedLog = 'Purchased: ' + output 
+  console.log(carPurchasedLog)
+  return carPurchasedLog
+}
+
+formatDateString()
+
+const lastPaymentTest = data[0].lastpayment
+
+// Challenge 3 
+function formatWhenBought() {
+  const output = new D(lastPaymentTest).when();
+  let 
+  console.log(output)
+}
+
+formatWhenBought()
+
